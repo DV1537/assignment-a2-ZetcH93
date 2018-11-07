6acbc6ad8745c416db246301cbbafd45ad80ed01
ZetcH93 <44842682+ZetcH93@users.noreply.github.com>
2018-11-07 20:34:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include "pch.h"
 #include <iostream>
 #include <fstream>
+
+using namespace std;
 
 /**
  * The program reads numbers from a file and prints the sum of them
@@ -8,26 +11,52 @@
  * @param  argv [description]
  * @return      [description]
  */
+
 int main(int argc, const char * argv[])
 {
-    
-    int a = 0;
-    int sum = 0;
-    
-    std::ifstream myReadFile;
-    
-    myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
-    {
-        sum += a;
-    }
-    myReadFile.close();
-    
-    
-    
-    std::cout << sum << "\n";
-    
-    return 0;
-}
 
+	int a = 0;
+	int sum = 0;
+	int counter = 0;
+	int* point = new int[counter];
+
+	std::ifstream myReadFile;
+
+	myReadFile.open(argv[1]);
+
+	if (myReadFile.is_open())
+	{
+		while (myReadFile >> a)
+		{
+			counter++;
+
+			int* tempArr = new int[counter];
+			for (int i = 0; i < counter; i++)
+				tempArr[i] = point[i];
+			delete[] point;
+			point = new int[counter];
+			for (int i = 0; i < counter; i++)
+				point[i] = tempArr[i];
+			delete[] tempArr;
+			tempArr = nullptr;
+
+
+			point[counter - 1] = a;
+			sum += a;
+
+		}
+		myReadFile.close();
+
+		for (int i = 0; i < counter; i++)
+			if (point[i] > (sum / counter))
+				cout << point[i] << " ";
+	}
+	else
+		cout << "Could not open file. \n";
+
+	delete[] point;
+	point = nullptr;
+
+
+	return 0;
+}
